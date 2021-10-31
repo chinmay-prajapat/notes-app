7ac4ca5c358545967890a38d8affae0729961e33
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-31 15:24:03 +0530

    inverse

--- chalk.js
+++ chalk.js
@@ -1,3 +1,5 @@
+const { inverse } = require("chalk");
 const chalk = require("chalk");
-const text = chalk.green(chalk.inverse.bold.bgYellow("Success"));
+const text = chalk.green(chalk.bold.bgYellow.red.inverse("Success"));
 module.exports = text;
+//inverse the style such as makes red to bg and yellow to text color
