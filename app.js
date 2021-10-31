0d62a797c8f736cd11d3e5c902c7a0924bfe859c
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-31 15:21:07 +0530

    chalk lib

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const validator = require("validator");
-
+const chalk = require("./chalk");
 const note = require("./notes");
 const { performance } = require("perf_hooks");
 var startTime = performance.now();
 
 console.log(note());
 console.log(validator.isEmail("c@gmal.com"));
+console.log(chalk);
 var endTime = performance.now();
 
 console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
---
+++ chalk.js
@@ -0,0 +1,3 @@
+const chalk = require("chalk");
+const text = chalk.green(chalk.inverse.bold.bgYellow("Success"));
+module.exports = text;
