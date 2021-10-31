fba5538fad1ee2a77e7a4580088cd4ce3b095a39
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-31 13:10:14 +0530

    validator package

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const add = require("./utils");
+const validator = require("validator");
+
+const note = require("./notes");
 const { performance } = require("perf_hooks");
 var startTime = performance.now();
-console.log(add(5, 5));
+
+note();
+console.log(validator.isEmail("c@gmal.com"));
 var endTime = performance.now();
 
 console.log(`Call to doSomething took ${endTime - startTime} milliseconds`);
---
+++ notes.js
@@ -0,0 +1,4 @@
+const noteFun = function () {
+  console.log("this is my note");
+};
+module.exports = noteFun;
